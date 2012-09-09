1e324bf3d4af4b19a5be412a75fdc2a2384f4669
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2012-09-09 00:49:41 -0700

    presence validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,7 @@ class Page < ActiveRecord::Base
   scope :published, where("published_on <= :current_datetime", current_datetime: DateTime.current).order("published_on DESC") 
   scope :unpublished, where("published_on IS NULL or published_on > :current_datetime", current_datetime: DateTime.current).order("published_on DESC") 
   validates_uniqueness_of :title
+  validates_presence_of :title, :content
 
   def total_words
     title.scan(/\w+/).size + content.scan(/\w+/).size
